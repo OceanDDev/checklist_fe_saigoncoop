e470afb55e910256b917f580993e9c2d93d0afee
duong.do <duong.binhdo@hitachids.com>
2025-11-01 08:41:40 +0700

    update routes

--- src/modules/Dieuvan/Toolrotkien/index.jsx
+++ src/modules/Dieuvan/Toolrotkien/index.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useMemo, useState, useCallback, useRef } from "react";
-import RotKienRow from "./RotKienRow";
 import KienHT from "./rotKienRow/kienHT";
 
 import { rotKienService } from "@/services/dieuvan/rotkien.service";
@@ -14,7 +13,8 @@ import ExcelJS from "exceljs";
 import AddKienDialog from "./addKien";
 import CustomPagination from "@/components/ui/customPagination";
 import AddCuaHangDialog from "./addCuaHang/AddCuaHangDialog";
-
+import RotKienRow from "./rotKienRow/RotKienRow";
+  
 // Helper: YYYY-MM-DD (local timezone)
 const toDateInputValue = (d = new Date()) => {
   const tz = d.getTimezoneOffset() * 60000;
